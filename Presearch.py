ea68fd82ecea868da02984d8ca64c118ccd943cd
gizlbizl <34004024+gizlbizl@users.noreply.github.com>
2023-03-18 18:17:20 +0300

    Update Presearch.py

--- Presearch.py
+++ Presearch.py
@@ -2,15 +2,17 @@ import pyautogui
 import webbrowser
 import time
 import os
+import random
 
-for i in range (30):
+for i in range (1):
     os.system("python3 config.py")
     url = 'https://www.presearch.com/'
     webbrowser.open_new(url)
     time.sleep(3)
-    f = open("log.txt", "r") 
-    pyautogui.typewrite(f.readline())
+    f = open("log.txt", "r").read().splitlines()
+    G = random.choice(f)
+    pyautogui.typewrite(G)
     time.sleep(3)
     pyautogui.hotkey('enter')
     time.sleep(5)
-os.system("taskkill /im brave.exe /f")
+os.system("taskkill /im msedge.exe /f")
